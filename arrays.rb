5fd0f428a1edf7c43a99bccd5bdb371cb295177c
Andrew Tsai <a168tsai@gmail.com>
2022-08-03 08:43:49 -0600

    Add understanding

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,4 @@
-#! the friends variable stores an array of strings. This array has 4 elements
+#! the friends variable stores an array of strings. this array has 4 elements
 friends = ["Andrew", "Cheuk", "Damir", "Paul"]
 
 #! the years variable stores an array of integers. this array has 4 elements
@@ -9,3 +9,8 @@ salary = [66.68, 88.86, 57.74, 86.78]
 
 #! the bffs variable stores an array of booleans. this array has 4 elements
 bffs = [false, true, true, false]
+
+#! there are 4 elements in the all of the Array.
+#! the index position of the String Andrew is 0.
+#! the value of the element in variable years in index position 2 is 23.
+#! the value of the last element in variable salary is 86.78
