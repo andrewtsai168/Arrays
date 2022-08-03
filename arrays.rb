a272da02b82fd1ea8f1ef2f6143fe18c6c69e8e8
Andrew Tsai <a168tsai@gmail.com>
2022-08-03 08:37:17 -0600

    Add explanation

--- arrays.rb
+++ arrays.rb
@@ -1,7 +1,11 @@
+#! the friends variable stores an array of strings. This array has 4 elements
 friends = ["Andrew", "Cheuk", "Damir", "Paul"]
 
+#! the years variable stores an array of integers. this array has 4 elements
 years = [ 14, 15, 23, 16]
 
+#! the salary variable stores an array of floats. this array has 4 elements
 salary = [66.68, 88.86, 57.74, 86.78]
 
+#! the bffs variable stores an array of booleans. this array has 4 elements
 bffs = [false, true, true, false]
